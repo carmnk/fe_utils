7e22edab0aeb335b96c9a5e2f80526adb1defd9e
carmnk <carso.menk@gmail.com>
2024-04-25 18:32:22 +0200

    init

--- src/components/inputs/index.tsx
+++ src/components/inputs/index.tsx
@@ -1,2 +1,7 @@
+import { ClickTextField } from './ClickTextField'
+export type { ClickTextFieldProps } from './ClickTextField'
 export { GenericInputField } from './GenericInputField'
 export type { GenericInputFieldProps } from './GenericInputField'
+
+console.log(ClickTextField)
+export { ClickTextField }
