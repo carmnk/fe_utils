8217962a2ca3f1ae261eb06ba5c93f3eb2e45b1b
carmnk <carso.menk@gmail.com>
2024-05-10 08:20:43 +0200

    0.1.25

--- src/components/inputs/index.tsx
+++ src/components/inputs/index.tsx
@@ -8,3 +8,30 @@ export type {
   SimpleGenericInputFieldProps,
   SpecificInputProps,
 } from './GenericInputField'
+
+export { TextField } from './TextField'
+export type { CTextFieldProps } from './TextField'
+
+export { NumberField } from './NumberField'
+export type { CNumberFieldProps } from './NumberField'
+
+export { Select } from './Select'
+export type { CSelectProps } from './Select'
+
+export { DatePicker } from './DatePicker'
+export type { DatePickerProps } from './DatePicker'
+
+export { TextArea } from './TextArea'
+export type { TextAreaProps } from './TextArea'
+
+export { Checkbox } from './Checkbox'
+export type { CheckboxProps } from './Checkbox'
+
+export { Switch } from './Switch'
+export type { CSwitchProps } from './Switch'
+
+export { MultiSelect } from './MultiSelect'
+export type { MultiSelectProps } from './MultiSelect'
+
+export { CAutoComplete } from './AutoComplete'
+export type { CAutoCompleteProps } from './AutoComplete'
