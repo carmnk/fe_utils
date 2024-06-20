cc624c9cdb22b3a7dafc4e32d5f0bce1feb72d5f
carmnk <carso.menk@gmail.com>
2024-06-20 10:45:39 +0200

    0.1.132

--- src/components/inputs/Select2.tsx
+++ src/components/inputs/Select2.tsx
@@ -1,8 +1,11 @@
-import { MenuItem, MenuItemProps } from '@mui/material'
+import { ListSubheader, MenuItem, MenuItemProps } from '@mui/material'
 import CTextField, { CTextFieldProps } from './TextField'
+import { Fragment, useMemo } from 'react'
+import { uniq } from 'lodash'
 
 export type CSelect2Props = CTextFieldProps & {
   options: { value: string | number | boolean; label: string }[]
+  groupBy?: (item: string | number | boolean) => string | number
   // onChange?: (newValue: string, e: React.ChangeEvent<HTMLInputElement>) => void
   slotProps?: CTextFieldProps['slotProps'] & {
     menuItem?: MenuItemProps
@@ -10,15 +13,39 @@ export type CSelect2Props = CTextFieldProps & {
 }
 
 export const CSelect2 = (props: CSelect2Props) => {
-  const { options, endIcon: _e, ...rest } = props
+  const { options, endIcon: _e, groupBy, ...rest } = props
   const menuItemProps = rest?.slotProps?.menuItem
+
+  const optionGroupNames = useMemo(() => {
+    return groupBy
+      ? uniq(options?.map?.((opt) => groupBy(opt.value)) ?? [])
+      : null
+  }, [groupBy, options])
+
   return (
     <CTextField {...rest} select>
-      {options?.map((opt, oIdx) => (
-        <MenuItem value={opt?.value as any} {...menuItemProps} key={oIdx}>
-          {opt.label}
-        </MenuItem>
-      ))}
+      {optionGroupNames?.length && groupBy
+        ? optionGroupNames.map((groupName, gIdx) => (
+            <Fragment key={gIdx}>
+              <ListSubheader>{groupName}</ListSubheader>
+              {options
+                ?.filter((opt) => groupBy(opt.value) === groupName)
+                .map((opt, oIdx) => (
+                  <MenuItem
+                    value={opt?.value as any}
+                    {...menuItemProps}
+                    key={oIdx}
+                  >
+                    {opt.label}
+                  </MenuItem>
+                ))}
+            </Fragment>
+          ))
+        : options?.map((opt, oIdx) => (
+            <MenuItem value={opt?.value as any} {...menuItemProps} key={oIdx}>
+              {opt.label}
+            </MenuItem>
+          ))}
     </CTextField>
   )
 }
