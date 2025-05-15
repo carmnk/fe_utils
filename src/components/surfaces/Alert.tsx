f9bedef82afd29d1ecad4d100661c12742e47020
carmnk <carso.menk@gmail.com>
2025-05-15 18:54:41 +0200

    0.3.5

--- src/components/surfaces/Alert.tsx
+++ src/components/surfaces/Alert.tsx
@@ -1,12 +1,15 @@
+import { ReactNode } from 'react'
 import { Modal } from './Modal'
 import { CModalProps } from './Modal'
 
-export const AlertDialog = (props: Omit<CModalProps, 'children'>) => {
+export const AlertDialog = (
+  props: Omit<CModalProps, 'children'> & { children?: ReactNode }
+) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { width, minWidth, ...rest } = props
+  const { width, minWidth, children, ...rest } = props
   return (
     <Modal width={788} minWidth={788} {...rest}>
-      <div></div>
+      {children}
     </Modal>
   )
 }
